3bb85ba92cffbcec47a5848c16d75b3ff8a70be3
Yuri Santos de Lima <yuri.y.u.r.i@hotmail.com>
2024-08-15 20:16:26 -0300

    modifica mensagem inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>awo, awooo</h1>;
+  return (
+    <div>
+      <h1>Clica nesse link aqui amor!</h1>
+      <a href="https://www.youtube.com/shorts/1FohgIClM-8">Awo, Awwoooo</a>
+    </div>
+  );
 }
 
 export default Home;
